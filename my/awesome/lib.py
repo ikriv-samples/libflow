77489562e96857d92c5d557a4e96f206dcf8300a
Ivan Krivyakov <ivan@ikriv.com>
2023-08-20 21:35:38 -0400

    Added random_digit() method

--- my/awesome/lib.py
+++ my/awesome/lib.py
@@ -1,3 +1,9 @@
+import random
+
 class MyAwesomeClass:
     def random_value(self):
         return 3 # chosen by a dice roll, guaranteed to be random
+
+    def random_digit(self):
+        return random.randint(0, 9)
+        
--- test/testlib.py
+++ test/testlib.py
@@ -1,9 +1,19 @@
+import random
 import unittest
+
+
 from my.awesome.lib import MyAwesomeClass
 
 class LibTest(unittest.TestCase):
     def test_random(self):
         self.assertEqual(3, MyAwesomeClass().random_value())
+
+    def test_really_random(self):
+        c = MyAwesomeClass()
+        random.seed(42)
+        self.assertEqual(1, c.random_digit())
+        self.assertEqual(0, c.random_digit())
+        self.assertEqual(4, c.random_digit())
 
 if __name__ == '__main__':
     unittest.main()
