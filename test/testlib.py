ca6b8ebebce151a3c00d5ed525fa19a791013368
Ivan Krivyakov <ivan@ikriv.com>
2023-08-20 21:26:37 -0400

    Fixed unit test

--- test/testlib.py
+++ test/testlib.py
@@ -3,7 +3,7 @@ from my.awesome.lib import MyAwesomeClass
 
 class LibTest(unittest.TestCase):
     def test_random(self):
-        self.assertEqual(4, MyAwesomeClass().random_value())
+        self.assertEqual(3, MyAwesomeClass().random_value())
 
 if __name__ == '__main__':
     unittest.main()
