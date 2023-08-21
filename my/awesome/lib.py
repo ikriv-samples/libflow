9b5b9d2dc7cc874ac520b1a5e26477f143b9c806
Ivan Krivyakov <ivan@ikriv.com>
2023-08-20 21:48:50 -0400

    Added docstrings

--- my/awesome/lib.py
+++ my/awesome/lib.py
@@ -3,9 +3,11 @@ import random
 class MyAwesomeClass:
     @staticmethod
     def random_value():
+        """Returns random integer value"""
         return 3 # chosen by a dice roll, guaranteed to be random
 
     @staticmethod
     def random_digit():
+        """Returns random decimal digit"""
         return random.randint(0, 9)
         
