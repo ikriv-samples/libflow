1ce9eeebe4a73c30f3a43dba2e380eb533200153
Ivan Krivyakov <ivan@ikriv.com>
2023-08-20 21:46:20 -0400

    Made methods static (breaking change)

--- my/awesome/lib.py
+++ my/awesome/lib.py
@@ -1,9 +1,11 @@
 import random
 
 class MyAwesomeClass:
-    def random_value(self):
+    @staticmethod
+    def random_value():
         return 3 # chosen by a dice roll, guaranteed to be random
 
-    def random_digit(self):
+    @staticmethod
+    def random_digit():
         return random.randint(0, 9)
         
--- test/testlib.py
+++ test/testlib.py
@@ -6,10 +6,10 @@ from my.awesome.lib import MyAwesomeClass
 
 class LibTest(unittest.TestCase):
     def test_random(self):
-        self.assertEqual(3, MyAwesomeClass().random_value())
+        self.assertEqual(3, MyAwesomeClass.random_value())
 
     def test_really_random(self):
-        c = MyAwesomeClass()
+        c = MyAwesomeClass
         random.seed(42)
         self.assertEqual(1, c.random_digit())
         self.assertEqual(0, c.random_digit())
