61d909dbb8c7f86798e486b8a0a7035569488261
Ivan Krivyakov <ivan@ikriv.com>
2023-08-20 21:48:50 -0400

    Added docstrings (v1.1.x)

--- my/awesome/lib.py
+++ my/awesome/lib.py
@@ -2,8 +2,10 @@ import random
 
 class MyAwesomeClass:
     def random_value(self):
+        """Returns random integer value"""
         return 3 # chosen by a dice roll, guaranteed to be random
 
     def random_digit(self):
+        """Returns random decimal digit"""
         return random.randint(0, 9)
         
